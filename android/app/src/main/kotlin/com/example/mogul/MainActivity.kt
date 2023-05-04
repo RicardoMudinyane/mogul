f7a80e19df30c04207096b1165e1b8d92df4531b
RicardoMudinyane <ricardomudinyane@gmail.com>
2023-05-04 13:31:47 +0200

    First Commit

---
+++ android/app/src/main/kotlin/com/example/mogul/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mogul
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color mainBlue = const Color(0xff0069f6);
+Color goldColor =const Color(0xffffd700);
+Color purpleColor =const Color(0xff663399);
+
+// FontStyle: Google Poppins
+// FontSize: 14-10,
